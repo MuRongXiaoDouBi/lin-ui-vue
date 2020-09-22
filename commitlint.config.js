d3d3cb810e8c6a3197e882701cd40f026dbc10f7
木荣 <372638156@qq.com>
2020-09-22 08:34:40 +0800

    feat(Husky): 新增 commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional'
+  ],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-case': [0, 'never'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'EsLint',
+        'Github',
+        'VuePress',
+        'package.json',
+        'License',
+        'TravisCI',
+        'GithubAction',
+        'Husky',
+        'README',
+        'util',
+        '.editorconfig',
+        '.gitignore',
+        'commitlint',
+        'Example'
+      ]
+    ]
+  }
+};
